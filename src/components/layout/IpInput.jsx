910e2f5948ebf8662002dde4128c65b8e78a258d
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-10-21 21:25:22 -0500

    Coded and implemented IpInput.

--- src/components/layout/IpInput.jsx
+++ src/components/layout/IpInput.jsx
@@ -1,20 +1,30 @@
 import React, { useState } from 'react'
+import { useDispatch } from "react-redux";
+import { setIp } from '../stateManagement/ipSlice';
 
 function IpInput() {
     const [input, setInput] = useState('')
 
+    const dispatch = useDispatch()
+
     function handleSubmit(event) {
         event.preventDefault()
-        console.log(input)
+        dispatch(
+            setIp(input)
+        )
     }
 
     return <section id='ip-input'>
         <h1>IP Address Tracker</h1>
 
         <form onSubmit={handleSubmit}>
+            {/* To add domain pattern */}
             <input
             type="text"
+            minLength="7"
+            maxLength="15"
             placeholder='Search for any IP address or domain'
+            pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$"
             onChange={event => setInput(event.target.value)} />
 
             <button type="submit">
